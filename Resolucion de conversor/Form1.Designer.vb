cd88ce2ec259e60927d428d717d5e512fca6ee3e
Carloschavarri <69088572+Carloschavarri@users.noreply.github.com>
2020-09-10 16:44:22 -0600

    Tarea de conversores

---
+++ Resolucion de conversor/Form1.Designer.vb
@@ -0,0 +1,160 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnConvertir = New System.Windows.Forms.Button()
+        Me.lblrespuesta = New System.Windows.Forms.Label()
+        Me.lblcantidad = New System.Windows.Forms.Label()
+        Me.txtcantidad = New System.Windows.Forms.TextBox()
+        Me.cbotipo = New System.Windows.Forms.ComboBox()
+        Me.lbltipo = New System.Windows.Forms.Label()
+        Me.cboa = New System.Windows.Forms.ComboBox()
+        Me.cbode = New System.Windows.Forms.ComboBox()
+        Me.lbla = New System.Windows.Forms.Label()
+        Me.lblde = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnConvertir
+        '
+        Me.btnConvertir.Font = New System.Drawing.Font("Arial", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnConvertir.Location = New System.Drawing.Point(111, 139)
+        Me.btnConvertir.Name = "btnConvertir"
+        Me.btnConvertir.Size = New System.Drawing.Size(172, 73)
+        Me.btnConvertir.TabIndex = 25
+        Me.btnConvertir.Text = "Convertir"
+        Me.btnConvertir.UseVisualStyleBackColor = True
+        '
+        'lblrespuesta
+        '
+        Me.lblrespuesta.AutoSize = True
+        Me.lblrespuesta.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblrespuesta.Location = New System.Drawing.Point(334, 167)
+        Me.lblrespuesta.Name = "lblrespuesta"
+        Me.lblrespuesta.Size = New System.Drawing.Size(111, 19)
+        Me.lblrespuesta.TabIndex = 24
+        Me.lblrespuesta.Text = "Respuesta: ?"
+        '
+        'lblcantidad
+        '
+        Me.lblcantidad.AutoSize = True
+        Me.lblcantidad.Location = New System.Drawing.Point(330, 88)
+        Me.lblcantidad.Name = "lblcantidad"
+        Me.lblcantidad.Size = New System.Drawing.Size(52, 13)
+        Me.lblcantidad.TabIndex = 23
+        Me.lblcantidad.Text = "Cantidad:"
+        '
+        'txtcantidad
+        '
+        Me.txtcantidad.Location = New System.Drawing.Point(388, 85)
+        Me.txtcantidad.Name = "txtcantidad"
+        Me.txtcantidad.Size = New System.Drawing.Size(100, 20)
+        Me.txtcantidad.TabIndex = 22
+        '
+        'cbotipo
+        '
+        Me.cbotipo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cbotipo.FormattingEnabled = True
+        Me.cbotipo.Items.AddRange(New Object() {"Monedas", "Longitud", "Masa", "Almacenamiento", "Tiempo"})
+        Me.cbotipo.Location = New System.Drawing.Point(203, 28)
+        Me.cbotipo.Name = "cbotipo"
+        Me.cbotipo.Size = New System.Drawing.Size(121, 21)
+        Me.cbotipo.TabIndex = 21
+        '
+        'lbltipo
+        '
+        Me.lbltipo.AutoSize = True
+        Me.lbltipo.Location = New System.Drawing.Point(166, 31)
+        Me.lbltipo.Name = "lbltipo"
+        Me.lbltipo.Size = New System.Drawing.Size(31, 13)
+        Me.lbltipo.TabIndex = 20
+        Me.lbltipo.Text = "Tipo:"
+        '
+        'cboa
+        '
+        Me.cboa.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cboa.FormattingEnabled = True
+        Me.cboa.Location = New System.Drawing.Point(203, 88)
+        Me.cboa.Name = "cboa"
+        Me.cboa.Size = New System.Drawing.Size(121, 21)
+        Me.cboa.TabIndex = 19
+        '
+        'cbode
+        '
+        Me.cbode.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cbode.FormattingEnabled = True
+        Me.cbode.Location = New System.Drawing.Point(53, 88)
+        Me.cbode.Name = "cbode"
+        Me.cbode.Size = New System.Drawing.Size(121, 21)
+        Me.cbode.TabIndex = 18
+        '
+        'lbla
+        '
+        Me.lbla.AutoSize = True
+        Me.lbla.Location = New System.Drawing.Point(180, 91)
+        Me.lbla.Name = "lbla"
+        Me.lbla.Size = New System.Drawing.Size(17, 13)
+        Me.lbla.TabIndex = 17
+        Me.lbla.Text = "A:"
+        '
+        'lblde
+        '
+        Me.lblde.AutoSize = True
+        Me.lblde.Location = New System.Drawing.Point(22, 91)
+        Me.lblde.Name = "lblde"
+        Me.lblde.Size = New System.Drawing.Size(25, 13)
+        Me.lblde.TabIndex = 16
+        Me.lblde.Text = "DE:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(524, 278)
+        Me.Controls.Add(Me.btnConvertir)
+        Me.Controls.Add(Me.lblrespuesta)
+        Me.Controls.Add(Me.lblcantidad)
+        Me.Controls.Add(Me.txtcantidad)
+        Me.Controls.Add(Me.cbotipo)
+        Me.Controls.Add(Me.lbltipo)
+        Me.Controls.Add(Me.cboa)
+        Me.Controls.Add(Me.cbode)
+        Me.Controls.Add(Me.lbla)
+        Me.Controls.Add(Me.lblde)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnConvertir As Button
+    Friend WithEvents lblrespuesta As Label
+    Friend WithEvents lblcantidad As Label
+    Friend WithEvents txtcantidad As TextBox
+    Friend WithEvents cbotipo As ComboBox
+    Friend WithEvents lbltipo As Label
+    Friend WithEvents cboa As ComboBox
+    Friend WithEvents cbode As ComboBox
+    Friend WithEvents lbla As Label
+    Friend WithEvents lblde As Label
+End Class
---
+++ Resolucion de conversor/Form1.vb
@@ -0,0 +1,70 @@
+﻿Public Class Form1
+    Dim objConversor = New conversores()
+
+    Private Sub cbotipo_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbotipo.SelectedIndexChanged
+        cbode.Items.Clear()
+        cbode.Text = ""
+        cbode.Items.AddRange(objConversor.etiquetas(cbotipo.SelectedIndex))
+
+        cboa.Items.Clear()
+        cboa.Text = ""
+        cboa.Items.AddRange(objConversor.etiquetas(cbotipo.SelectedIndex))
+    End Sub
+
+    Private Sub btnConvertir_Click(sender As Object, e As EventArgs) Handles btnConvertir.Click
+        lblrespuesta.Text = "Respuesta: " + objConversor.convertir(cbotipo.SelectedIndex, cbode.SelectedIndex, cboa.SelectedIndex, txtcantidad.Text).ToString() + " " + objConversor.etiquetas(cbotipo.SelectedIndex)(cboa.SelectedIndex)
+    End Sub
+
+    Private Sub lblrespuesta_Click(sender As Object, e As EventArgs) Handles lblrespuesta.Click
+
+    End Sub
+
+    Private Sub lblcantidad_Click(sender As Object, e As EventArgs) Handles lblcantidad.Click
+
+    End Sub
+
+    Private Sub txtcantidad_TextChanged(sender As Object, e As EventArgs) Handles txtcantidad.TextChanged
+
+    End Sub
+
+    Private Sub lbltipo_Click(sender As Object, e As EventArgs) Handles lbltipo.Click
+
+    End Sub
+
+    Private Sub cboa_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboa.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub cbode_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbode.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub lbla_Click(sender As Object, e As EventArgs) Handles lbla.Click
+
+    End Sub
+
+    Private Sub lblde_Click(sender As Object, e As EventArgs) Handles lblde.Click
+
+    End Sub
+End Class
+
+Class conversores
+    Public etiquetas()() As String = {
+        New String() {"Dolar", "Euro", "Quetzales", "Lempira", "Colon SV", "Cordoba", "Colon CR"},
+        New String() {"Metro", "CM", "Pulgadas", "Pies", "Varas", "Yardas", "Km", "Millas"},
+        New String() {"Libra", "Gramos", "Kilogramos", "Onzas", "Quintales", "Toneladas"},
+        New String() {"MB", "Bit", "Byte", "KB", "GB", "TB"},
+        New String() {"Hora", "Segundos", "Minutos", "Dias", "Semana", "Meses", "Años"}
+    }
+    Public valores()() As Double = {
+        New Double() {1, 0.85, 7.74, 24.8, 8.75, 34.6, 597.23},
+        New Double() {1, 100, 39.3701, 3.28084, 1.1963081929167, 1.09361, 0.001, 0.000621371},
+        New Double() {1, 453.592, 0.453592, 16, 0.01, 0.000453592},
+        New Double() {1, 8000000.0, 1000000.0, 1000, 0.001, 0.000001},
+        New Double() {1, 3600, 60, 0.0416667, 0.00595238, 0.00136986, 0.000114155}
+    }
+    Public Function convertir(ByVal tipo As Int16, ByVal de As Int16, ByVal a As Int16, ByVal cantidad As Double)
+        Return valores(tipo)(a) / valores(tipo)(de) * cantidad
+    End Function
+End Class
+
